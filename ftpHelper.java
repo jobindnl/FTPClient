57613758dffb87595b1b27e57b10a05503605145
jobindnl <jobindnl@gmail.com>
2020-09-26 19:43:43 -0400

    added put method

--- ftpHelper.java
+++ ftpHelper.java
@@ -78,6 +78,53 @@ public class ftpHelper {
         
     }
 
+    public void PUT(String address){
+
+        String input;
+        try {
+            out.println("PASV");
+            out.flush();
+            String portnum = in.readLine();
+            int index = portnum.indexOf("(") + 1;
+            int indexLast = portnum.indexOf(")");
+            String newStr = portnum.substring(index, indexLast);
+            portData = newStr.split(",");
+            
+            int port1, port2;
+            port1 = Integer.parseInt(portData[4]);
+            port2 = Integer.parseInt(portData[5]);
+            int newPort = (port1 * 256) + port2;
+            
+            String hostNumberIP = portData[0] + "." + portData[1] + "." +portData[2] + "." + portData[3];
+            
+            sock2 = new Socket(hostNumberIP, newPort);
+            //in2 = new BufferedReader(new InputStreamReader(sock2.getInputStream()));
+            out.println("STOR " + address);
+            out.flush();
+            String line = in.readLine();
+            //System.out.println(in.readLine());
+
+            File file = new File(address);
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            BufferedOutputStream out = new BufferedOutputStream(sock2.getOutputStream());
+            byte[] buffer = new byte[8192];
+            int count;
+            while ((count = bis.read(buffer)) > 0) {
+                out.write(buffer, 0, count);
+
+            }
+            out.close();
+            fis.close();
+            bis.close();
+    
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
     public void GET(String address){
 
         String input;
