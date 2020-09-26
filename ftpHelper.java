a5924fc16306f90412cb7428e7aa9a5612130312
rioluisma <44717081+rioluisma@users.noreply.github.com>
2020-09-26 19:08:55 -0400

    Update ftpHelper.java
    Login method added.
    Prompts the user for the User name and password to then send it to the Server to validate and stablish connection and reads the Server reply message.

--- ftpHelper.java
+++ ftpHelper.java
@@ -14,5 +14,25 @@ public class ftpHelper {
 
     
 
+    public boolean LOGIN(){
+
+        try {
+        System.out.print("Enter username: ");
+        String userName = user.readLine();
+        out.println("USER " + userName);
+        out.flush();
+        System.out.print(in.readLine() + ": ");
+        String password = user.readLine();
+
+        //System.out.println(in.readLine());
+        out.println("PASS " + password);
+        out.flush();
+        System.out.println(in.readLine());
+             } catch (Exception e) {
+            System.out.println(e);
+        }
+        return false;
+     }
+
 
 }
