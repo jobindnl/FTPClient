9847c88097b80939fd78e4a8bdafed8219c1d887
jobindnl <jobindnl@gmail.com>
2020-09-26 18:15:03 -0400

    added buffered reader for user input

--- ftpClient.java
+++ ftpClient.java
@@ -23,7 +23,25 @@ public class ftpClient {
 			sock = new Socket(host, 21);
 			PrintWriter out = new PrintWriter(sock.getOutputStream());
 			BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			BufferedReader user = new BufferedReader(new InputStreamReader(System.in));
 
+			/**
+			 * creates new object of helper class with all methods
+			 * passes all printwriter and bufferedReaders
+			 */
+			
+			 
+			System.out.println("Connection Established ");
+			System.out.println(in.read()); //reads first line upon connection
+
+			String input; //string for user commands
+			boolean flag = true; //prevents user from logging in again
+
+			while(true){
+				System.out.print("Enter Command: ");
+				input = 
+			}
+			 
 
 		} catch(Exception e){
 			System.out.println(e);
