5b5d121ce6aadd90df84665715685c993abd9299
rioluisma <44717081+rioluisma@users.noreply.github.com>
2020-09-26 19:00:53 -0400

    Add files via upload
    Ste up variable instances and add the constructor

---
+++ ftpHelper.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.io.*;
+
+
+public class ftpHelper {
+    
+    private PrintWriter out;
+    private BufferedReader in;
+    private PrintWriter out2;
+    private BufferedReader in2;
+    private Socket sock2;
+    private BufferedReader user;
+    String portData[] =  new String[30];
+
+    
+
+
+}
